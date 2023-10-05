49d268b90a56fcc1299338628a9090bdcba9d033
unknown <NullRin@users.noreply.github.com>
2023-10-04 21:21:16 -0700

    add image crate to load image details

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use image::GenericImageView;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Enter file path:");
+    
+    let mut file_name = String::new();
+
+    io::stdin()
+        .read_line(&mut file_name)
+        .expect("Failed to read line");
+
+
+    let img = image::open(file_name.trim()).unwrap();
+
+    // The dimensions method returns the images width and height.
+    println!("dimensions {:?}", img.dimensions());
+
+    // The color method returns the image's `ColorType`.
+    println!("{:?}", img.color());
 }
